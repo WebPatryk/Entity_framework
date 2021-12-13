416d8d5f01c23d44770b9e31263994958f8033ab
Patryk Lekki <patryk.lekki@awesome-ind.com>
2021-12-13 22:58:51 +0100

    Model additional information

--- Models/Games.cs
+++ Models/Games.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace Entity_framework.Models
@@ -11,9 +12,14 @@ namespace Entity_framework.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        [DataType(DataType.Date)] //we need Date only
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
         public int Rate { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
